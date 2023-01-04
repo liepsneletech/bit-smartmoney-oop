8d9a5f053fcd805781b8c2b98b230f8062705f83
liepsnele <liepsnele.tech@gmail.com>
2023-01-04 15:48:51 +0200

    Update

--- public/index.php
+++ public/index.php
@@ -6,4 +6,24 @@ use App\Controllers\SitesController;
 
 $app = new SitesController;
 
-echo $app->greet();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Blaaaaa</title>
+  <link href="../node_modules/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="./assets/css/test.css">
+</head>
+
+<body>
+  tgtgtgtgt
+  <i class="fa-brands fa-facebook"></i>
+
+</body>
+
+</html>
