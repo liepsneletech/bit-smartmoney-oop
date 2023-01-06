f34fb367eb732295c39bc954adcf330de68172b1
liepsnele <liepsnele.tech@gmail.com>
2023-01-06 10:34:37 +0200

    Update

--- public/index.php
+++ public/index.php
@@ -2,9 +2,10 @@
 
 require __DIR__ . '/../vendor/autoload.php';
 
+use app\Controllers\Application;
 use App\Controllers\SitesController;
 
-$app = new SitesController;
+$app = new Application();
 
 ?>
 
@@ -15,7 +16,7 @@ $app = new SitesController;
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Blaaaaa</title>
+  <title>Smartmoney | Accounts</title>
   <link href="./node_modules/@fortawesome/fontawesome-free/css/all.min.css" rel="stylesheet">
   <link rel="stylesheet" href="./assets/css/test.css">
 </head>
